33d71076d911616a2783da24a9f16b912f48c710
Hiroyuki Okamura <okamu@hiroshima-u.ac.jp>
2021-10-05 01:32:19 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,9 @@
 echo "$RSTUDIO_USER:$RSTUDIO_PASSWORD" | chpasswd
 
 if [ $RSTUDIO_GRANT_SUDO = "yes" ]; then
-  echo "rstudio ALL=(ALL) ALL" >> /etc/sudoers.d/rstudio
+  echo "$RSTUDIO_USER ALL=(ALL) ALL" >> /etc/sudoers.d/$RSTUDIO_USER
 elif [ $RSTUDIO_GRANT_SUDO = "nopass" ]; then
-  echo "rstudio ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/rstudio
+  echo "$RSTUDIO_USER ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/$RSTUDIO_USER
 fi
 
 ## Rstudio
